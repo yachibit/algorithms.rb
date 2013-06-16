d889f2c1d78854a04a7bc5a1f871316eda9a714e
yachibit <y.yachix@gmail.com>
2013-06-16 18:41:19 +0900

    merge sort

--- lib/algorithms/sort.rb
+++ lib/algorithms/sort.rb
@@ -45,4 +45,20 @@ module Algorithms::Sort
     end
     container
   end
+
+  def self.mergesort(container)
+    return container if container.size <= 1
+    mid = container.size / 2
+    left = container[0...mid]
+    right = container[mid...container.size]
+    merge(mergesort(left), mergesort(right))
+  end
+
+  def self.merge(left, right)
+    sorted = []
+    until left.empty? or right.empty?
+      left.first <= right.first ? sorted << left.shift : sorted << right.shift
+    end
+    sorted + left + right
+  end
 end
